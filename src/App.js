7421c203b938ec8f8905557cbc3521b226289181
AdiPandey45 <adipandey1405@gmail.com>
2025-02-18 01:40:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
+const[text,setText]=useState('');
+
+
+//variation-1 = rendered again and again as you make chnages to the component
+// useEffect( ()=>{
+//   console.log("ui rendering done");
+// });
+
+// // //variation-2 = rendered only once
+// // useEffect ( ()=>{
+// //   console.log("ab text bhi print hoga");
+// // },[]);
+
+
+const changeHandler = (event) => {
+  console.log(event.target.value);
+  setText(event.target.value); 
+};
+
+//variation-3 = rendered or console the part only when you are dealing with the specific part of the component
+// useEffect(()=>{
+//   console.log("rishta pakka");
+// },[text]);
+
+
+//variation-4
+
+useEffect(()=>{
+  //add event listener
+  console.log("listener added");
+
+  return ()=>{
+    console.log("listner removed");
+  }
+},[text]);
+ 
+
+
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input type="text" onChange={changeHandler}></input>
     </div>
   );
 }
